5f7a234e5f0ae2dd50e81d5da32fe02aac69acf6
reszczak <reszczak@gmail.com>
2022-12-27 21:44:52 +0100

    Ify Zmiany
    Już jakoś idzie, trzeba jeszcze zrobic ify na bilans (bo moze byc taki sam)
    
    Próba zrobienia ifów na wyświetlanie tabeli
    
    Chce zrobic z mniejsza ilością ifów ale narazie nic nie moge innego wymyslec.

--- ProjektZaliczeniowy.cpp
+++ ProjektZaliczeniowy.cpp
@@ -4,9 +4,232 @@
 #include <stdio.h>      
 #include <stdlib.h>     
 #include <time.h> 
+#include <conio.h>
 using namespace std;
 
+class grupa
+{
+public:
+    string dr1, dr2, dr3, dr4;
+    int pkt1, pkt2, pkt3, pkt4;
+    int bil1, bil2, bil3, bil4;
+    string group;
+    void druzyna1() 
+    {
+        cout << dr1 << " " << pkt1 << " " << bil1 << endl;;
+    }
+    void druzyna2()
+    {
+        cout << dr2 << " " << pkt2 << " " << bil2 << endl;;
+    }
+    void druzyna3()
+    {
+        cout << dr3 << " " << pkt3 << " " << bil3 << endl;;
+    }
+    void druzyna4()
+    {
+        cout << dr4 << " " << pkt4 << " " << bil4 << endl;;
+    }
+    
+    void tabela() 
+    {
+        if (dr1>dr2>dr3>dr4)
+        {
+            druzyna1();
+            druzyna2();
+            druzyna3();
+            druzyna4();
+           
+        }
+        if (dr1 > dr2 > dr4 > dr3)
+        {
+            druzyna1();
+            druzyna2();
+            druzyna4();
+            druzyna3();
 
+        }
+        if (dr1 > dr3 > dr2 > dr4)
+        {
+            druzyna1();
+            druzyna3();
+            druzyna2();
+            druzyna4();
+
+        }
+        if (dr1 > dr3 > dr4 > dr2)
+        {
+            druzyna1();
+            druzyna3();
+            druzyna4();
+            druzyna2();
+
+        }
+        if (dr1 > dr4 > dr3 > dr2)
+        {
+            druzyna1();
+            druzyna4();
+            druzyna3();
+            druzyna2();
+
+        }
+        if (dr1 > dr4 > dr2 > dr3)
+        {
+            druzyna1();
+            druzyna4();
+            druzyna2();
+            druzyna3();
+
+        }
+        if (dr2 > dr1 > dr3 > dr4)
+        {
+            druzyna2();
+            druzyna1();
+            druzyna3();
+            druzyna4();
+
+        }
+        if (dr2 > dr1 > dr4 > dr3)
+        {
+            druzyna2();
+            druzyna1();
+            druzyna4();
+            druzyna3();
+
+        }
+        if (dr2 > dr3 > dr1 > dr4)
+        {
+            druzyna2();
+            druzyna3();
+            druzyna1();
+            druzyna4();
+
+        }
+        if (dr2 > dr3 > dr4 > dr1)
+        {
+            druzyna2();
+            druzyna3();
+            druzyna4();
+            druzyna1();
+
+        }
+        if (dr2 > dr4 > dr3 > dr1)
+        {
+            druzyna2();
+            druzyna4();
+            druzyna3();
+            druzyna1();
+
+        }
+        if (dr2 > dr4 > dr1 > dr3)
+        {
+            druzyna2();
+            druzyna4();
+            druzyna1();
+            druzyna3();
+
+        }
+        if (dr3 > dr1 > dr2 > dr4)
+        {
+            druzyna3();
+            druzyna1();
+            druzyna2();
+            druzyna4();
+
+        }
+        if (dr3 > dr1 > dr4 > dr2)
+        {
+            druzyna3();
+            druzyna1();
+            druzyna4();
+            druzyna2();
+
+        }
+        if (dr3 > dr2 > dr1 > dr4)
+        {
+            druzyna3();
+            druzyna2();
+            druzyna1();
+            druzyna4();
+
+        }
+        if (dr3 > dr2 > dr4 > dr1)
+        {
+            druzyna3();
+            druzyna2();
+            druzyna4();
+            druzyna1();
+
+        }
+        if (dr3 > dr4 > dr2 > dr1)
+        {
+            druzyna3();
+            druzyna4();
+            druzyna2();
+            druzyna1();
+
+        }
+        if (dr3 > dr4 > dr1 > dr2)
+        {
+            druzyna3();
+            druzyna4();
+            druzyna1();
+            druzyna2();
+
+        }
+        if (dr4 > dr1 > dr2 > dr3)
+        {
+            druzyna4();
+            druzyna1();
+            druzyna2();
+            druzyna3();
+
+        }
+        if (dr4 > dr1 > dr3 > dr2)
+        {
+            druzyna4();
+            druzyna1();
+            druzyna3();
+            druzyna2();
+
+        }
+        if (dr4 > dr2 > dr1 > dr3)
+        {
+            druzyna4();
+            druzyna2();
+            druzyna1();
+            druzyna3();
+
+        }
+        if (dr4 > dr2 > dr3 > dr1)
+        {
+            druzyna4();
+            druzyna2();
+            druzyna3();
+            druzyna1();
+
+        }
+        if (dr4 > dr3 > dr2 > dr1)
+        {
+            druzyna4();
+            druzyna3();
+            druzyna2();
+            druzyna1();
+
+        }
+        if (dr4 > dr3 > dr1 > dr2)
+        {
+            druzyna4();
+            druzyna3();
+            druzyna1();
+            druzyna2();
+
+        }
+    
+    }
+
+
+};
 //
 //int bramki()
 //{
@@ -37,7 +260,7 @@ int main()
 {
     int a = 4;
     int b = 6;
-    cout << "szansa pierwszego zespolu to: " << szansa1(a ,b) << "%" << endl;
+    cout << "szansa pierwszego zespolu to: " << szansa1(a, b) << "%" << endl;
     cout << "szansa drugiego zespolu to: " << szansa2(a, b) << "%"<<endl;
 
 }
