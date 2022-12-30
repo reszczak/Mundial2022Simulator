df71f40bc896c5b32bb895a2a197446921e52d95
cymo26 <121673840+cymo26@users.noreply.github.com>
2022-12-30 14:47:13 +0100

    Naprawienie błędu z szansami
    Udalo sie naprawic bledy zwiazanie z szansami takze teraz wszystko dziala elegancko

--- ProjektZaliczeniowy.cpp
+++ ProjektZaliczeniowy.cpp
@@ -8,25 +8,25 @@ using namespace std;
 
 
 
-//int bramki()
-//{
-//    int bramki;
-//    srand(time(NULL));
-//    bramki = rand() % 6 + 1;
-//    return bramki;
-//}
-//double szansa1(int waga1,int waga2)
-//{
-//   double szansa1;
-//    szansa1 = (waga1 / (waga1 + waga2))*100;
-//   return szansa1;
-//}
-//double szansa2(int waga1,int waga2)
-//{
-//    double szansa2;
-//    szansa2 = (waga2/(waga1 + waga2))*100;
-//    return szansa2;
-//}
+int bramki()
+{
+    int bramki;
+    srand(time(NULL));
+    bramki = rand() % 6 + 1;
+    return bramki;
+}
+double szansa1(double waga1,double waga2)
+{
+   double szansa1;
+    szansa1 = (waga1 / (waga1 + waga2))*100;
+   return szansa1;
+}
+double szansa2(double waga1,double waga2)
+{
+    double szansa2;
+    szansa2 = (waga2/(waga1 + waga2))*100;
+    return szansa2;
+}
 //int mecz1(int dr1, int dr2) 
 //{
 //    int dr1p, dr2p, dr1m, dr2m;
@@ -35,8 +35,8 @@ using namespace std;
 
 int main()
 {
-    int a = 4;
-    int b = 6;
+    double a = 4;
+    double b = 6;
     cout << "szansa pierwszego zespolu to: " << szansa1(a ,b) << "%" << endl;
     cout << "szansa drugiego zespolu to: " << szansa2(a, b) << "%"<<endl;
 
